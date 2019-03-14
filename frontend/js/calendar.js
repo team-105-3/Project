fa996697f055c75f2f60f0055befe2d061dee58b
Lara Chunko <lara@engr2-1-8-217-edu.int.colorado.edu>
2019-03-14 10:10:35 -0600

    working on calendar.js displayDate()

--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -12,6 +12,41 @@ function clock()
     document.getElementById("date").innerHTML=month+" "+year;
 }
 
+function displayDate()
+{
+    var currentDate = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var dayOfWeek = new Date().getDay()+1;
+
+    switch(dayOfWeek) {
+        case 1: <!-- Sunday -->
+            // code block
+            break;
+        case 2: <!-- Monday -->
+            // code block
+            break;
+        case 3: <!-- Tuesday -->
+            // code block
+            break;
+        case 4: <!-- Wednesday -->
+            // code block
+            break;
+        case 5: <!-- Thursday -->
+            // code block
+            break;
+        case 6: <!-- Friday -->
+            // code block
+            break;
+        case 7: <!-- Saturday -->
+            // code block
+            break;
+        default:
+        // code block
+    }
+
+    document.getElementById("Sunday").innerHTML= "Sunday" + " " + month + "/" + currentDate;
+}
+
 
 
 
