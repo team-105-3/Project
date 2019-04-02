a3b81c0cb6ef8300ab999f4c0982e93a72d01078
villyqer <47004155+villyqer@users.noreply.github.com>
2019-04-02 16:18:42 -0600

    update

--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -1,148 +1,5 @@
-$.fn.modal.Constructor.prototype.enforceFocus = function () {};
-
-/**
- * Creates an event and sends it to the server.
- */
-function createCalendarEvent() {
-    var title = document.getElementById('e_title').value;
-    var startTime = document.getElementById('est').value;
-    var endTime = document.getElementById('eet').value;
-    var recurring = document.getElementById('recurring').checked;
-    var startDate = document.getElementById('esd').value;
-    var endDate = document.getElementById('ed').value;
-    var desc = document.getElementById('e_desc').value;
-
-    var rec = document.getElementById('rec').value;
-    var timeframe = document.getElementById('timeframe').value;
-
-    console.log(rec, timeframe);
-
-    var fail = false;
-    if(emptyString(title) || emptyString(startTime) || emptyString(endTime) || emptyString(startDate)) {
-        fail = true;
-    }
-
-    if(recurring && (emptyString(endDate) || rec == "Choose..." || timeframe == "Choose...")) {
-        fail = true;
-    }
-
-    $('#createEventModal').modal('toggle');
-    clearAllEntries();
-
-    if(fail) {
-        $('#createEventModal').on('hidden.bs.modal', function() {
-            $('#eventCreationFail').modal('toggle');
-        });
-        return;
-    }
-
-    var event = new Event(title, startTime, endTime, recurring, startDate, endDate, desc, rec, timeframe);
-
-    var idKey = new URL(window.location.href).searchParams.get('key');
-
-    var sendObj = {key: idKey, newEvent: event};
-
-
-    //create ajax request
-    var request = new XMLHttpRequest();
-
-    var url = connectUrl + "/createEvent/";
-
-    //open up a post requst to defined url
-    request.open('POST', url);
-    request.setRequestHeader("Content-type", "application/json");
-
-    //we will be getting a text response from server
-    request.responseType = "json";
-
-    //when we have gotten a response from the server, print it to the console
-    request.onload = function() {
-        //$('#loading').modal('hide');
-        var status = request.response.status;
-        if(status == 1) {
-            console.log("successfully created event");
-            user.addEvent(event);
-        } else {
-            console.log('failed to create event');
-        }
-    }
-
-    request.send(JSON.stringify(sendObj));
-    //$('#loading').modal('show');
-
-}
-
-/**
- * Creates a project and sends it to the server
- */
-function createCalendarProject() {
-    var title = document.getElementById('p_title').value;
-    var startDate = document.getElementById('pst').value;
-    var dueDate = document.getElementById('pet').value;
-    var expTimeHours = document.getElementById('p_hours').value;
-    var expTimeMin = document.getElementById('p_min').value;
-    var desc = document.getElementById('p_desc').value;
-
-    var fail = false;
-    if(emptyString(title) || emptyString(startDate) || emptyString(dueDate) || emptyString(expTimeHours) || emptyString(expTimeMin)) {
-        fail = true;
-    }
-
-    $('#createProjectModal').modal('toggle');
-    clearAllEntries();
-
-    if(fail) {
-        $('#createProjectModal').on('hidden.bs.modal', function() {
-            $('#projectCreationFail').modal('toggle');
-        });
-        return;
-    }
-
-    var project = new Project(title, startDate, dueDate, expTimeHours, expTimeMin, desc);
-
-    var idKey = new URL(window.location.href).searchParams.get('key');
-
-    var sendObj = {key: idKey, newProject: project};
-
-
-    //create ajax request
-    var request = new XMLHttpRequest();
-
-    var url = connectUrl + "/createProject/";
-
-    //open up a post requst to defined url
-    request.open('POST', url);
-    request.setRequestHeader("Content-type", "application/json");
-
-    //we will be getting a text response from server
-    request.responseType = "json";
-
-    //when we have gotten a response from the server, print it to the console
-    request.onload = function() {
-        //$('#loading').modal('hide');
-        var status = request.response.status;
-        if(status == 1) {
-            console.log("Successfully created project");
-            user.addProject(project);
-        } else {
-            console.log("Failed to create project");
-        }
-    }
-
-    request.send(JSON.stringify(sendObj));
-    //$('#loading').modal('show');
-}
-
-/**
- * Clears all entries in every form.
- */
-function clearAllEntries() {
-    var entries = document.querySelectorAll('.entry');
-    entries.forEach(element => {
-        element.value = '';
-    });
 var background = document.body.style.backgroundColor;
-
+var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 function clock()
 {
     var d = new Date();
@@ -161,31 +18,31 @@ function displayDate()
     var dayOfWeek = new Date().getDay()+1;
     var weekarr= ["Sun", "Mon", "Tues", "Wedn", "Thurs", "Fri", "Sat"];
     switch(dayOfWeek) {
-        case 1: //<!-- Sunday -->
+        case 1: <!-- Sunday -->
             var i = 0;
             var trackDay = 0;
             for(i =0; i < 7; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
             break;
-        case 2: //<!-- Monday -->
+        case 2: <!-- Monday -->
 
             var i = 0;
             var trackDay = 0;
             for(i =-1; i < 6; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
 
             break;
-        case 3: //<!-- Tuesday -->
+        case 3: <!-- Tuesday -->
             // code block
 
             var i = 0;
@@ -193,14 +50,14 @@ function displayDate()
             for(i =-2; i < 5; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
 
 
             break;
-        case 4: //<!-- Wednesday -->
+        case 4: <!-- Wednesday -->
             // code block
 
 
@@ -209,24 +66,24 @@ function displayDate()
             for(i =-3; i < 4; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
             break;
-        case 5: //<!-- Thursday -->
+        case 5: <!-- Thursday -->
             // code block
             var i = 0;
             var trackDay = 0;
             for(i =-4; i < 3; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
             break;
-        case 6: //<!-- Friday -->
+        case 6: <!-- Friday -->
             // code block
 
 
@@ -235,14 +92,14 @@ function displayDate()
             for(i =-5; i < 2; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
 
 
             break;
-        case 7: //<!-- Saturday -->
+        case 7: <!-- Saturday -->
             // code block
 
 
@@ -251,7 +108,7 @@ function displayDate()
             for(i =-6; i < 1; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
-                document.getElementById(day).innerHTML= weekarr[trackDay] + " " + month + "/" + date;
+                document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
 
@@ -261,13 +118,99 @@ function displayDate()
     }
 }
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = background;
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
 }
