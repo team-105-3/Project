1ac4666d2e297f109d0df15196e0b181bd385597
villyqer <47004155+villyqer@users.noreply.github.com>
2019-04-03 16:36:44 -0600

    js update

--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -1,121 +1,170 @@
-var background = document.body.style.backgroundColor;
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-function clock()
-{
-    var d = new Date();
-    var date = d.getDate();
-    var year = d.getFullYear();
-    var month = d.getMonth();
-    var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+var date = new Date().getDate();
+var week = new Date().getDay();
+var month = new Date().getMonth();
+var year = new Date().getFullYear();
+var currentDate = date;
+
+var weekarr= ["Sun", "Mon", "Tues", "Wedn", "Thurs", "Fri", "Sat"];
+var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+var month30 = ["April", "June", "September", "November"];
+var month31 = ["January", "March", "May", "July", "August", "October", "December"];
+
+function view_switcher(){
+    var header = document.getElementById("myDIV");
+    var btns = header.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++){
+        btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+}
+
+function clock(){
     month = monthArr[month];
     document.getElementById("date").innerHTML=month+" "+year;
 }
 
-function displayDate()
+function fixweek(date,month){
+    if(month=="February"){
+        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+            if(date > 29){
+                date = 1;
+                month = "March";
+            }
+        }
+        else{
+            if(date > 28){
+                date = 1;
+                month = "March";
+            }
+        }
+    }
+    else if(month31.includes(month)){
+        if(date > 31){
+            date = 1;
+            month = monthArr[monthArr.findIndex(month) + 1];
+        }
+    }
+    else{
+        if(date > 30){
+            date = 1;
+            console.log(month);
+            console.log(monthArr.findIndex(month));
+            month = monthArr[monthArr.findIndex(month) + 1];
+        }
+    }
+}
+
+function createweek()
 {
-    var currentDate = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var dayOfWeek = new Date().getDay()+1;
-    var weekarr= ["Sun", "Mon", "Tues", "Wedn", "Thurs", "Fri", "Sat"];
-    switch(dayOfWeek) {
-        case 1: <!-- Sunday -->
+    console.log(currentDate);
+    console.log(week);
+    switch(week) {
+        case 0: <!-- Sunday -->
             var i = 0;
             var trackDay = 0;
             for(i =0; i < 7; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
             break;
-        case 2: <!-- Monday -->
+
+        case 1: <!-- Monday -->
 
             var i = 0;
             var trackDay = 0;
             for(i =-1; i < 6; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
-
             break;
-        case 3: <!-- Tuesday -->
-            // code block
 
+        case 2: <!-- Tuesday -->
+            // code block
             var i = 0;
             var trackDay = 0;
             for(i =-2; i < 5; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
-
-
             break;
-        case 4: <!-- Wednesday -->
+
+        case 3: <!-- Wednesday -->
             // code block
-
-
             var i = 0;
             var trackDay = 0;
             for(i =-3; i < 4; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
             break;
-        case 5: <!-- Thursday -->
+
+        case 4: <!-- Thursday -->
             // code block
             var i = 0;
             var trackDay = 0;
             for(i =-4; i < 3; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
             break;
-        case 6: <!-- Friday -->
+
+        case 5: <!-- Friday -->
             // code block
-
-
             var i = 0;
             var trackDay = 0;
             for(i =-5; i < 2; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
-
-
             break;
-        case 7: <!-- Saturday -->
+
+        case 6: <!-- Saturday -->
             // code block
-
-
             var i = 0;
             var trackDay = 0;
             for(i =-6; i < 1; i++) {
                 var day = weekarr[trackDay];
                 var date = currentDate + i;
+                fixweek(date,month);
                 document.getElementById(day).innerHTML= weekarr[trackDay] + "<br />" + month + "/" + date;
                 trackDay += 1;
             }
-
             break;
+
         default:
         // code block
     }
+}
+function displayDate(number)
+{
+    currentDate = currentDate+ (number*7);
+    
+    //console.log(currentDate);
+    //console.log(number);
+
+    createweek();
+    
 }
 
 function autocomplete(inp, arr) {
