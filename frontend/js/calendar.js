e36d906e16565df887110d7407a072a43a4fa1c0
Nick Elsasser <elsanic96@gmail.com>
2019-04-18 19:47:05 -0600

    Added project planning, still need to fix lots of bugs, fixed bug in email reminders

--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -159,7 +159,7 @@ function createCalendarProject(callback) {
         return;
     }
 
-    var project = new Project(title, startDate, dueDate, expTimeHours, expTimeMin, desc);
+    var project = new Project(title, startDate, dueDate, expTimeHours, expTimeMin, desc, expTimeHours * 60 + expTimeMin);
 
     var idKey = new URL(window.location.href).searchParams.get('key');
 
@@ -355,6 +355,223 @@ function displayUsersEvents(user) {
     }
 }
 
+/**
+ * Calculates where to allocate projects for a certain day
+ * 
+ * @param {*} user 
+ * @param {*} date 
+ */
+function calcProjectTimeSlotsForDay(user, date) {
+    var dayEvents = user.getAllEventsOnDay(date);
+    var dayProjects = user.getAllProjectsOnDay(date);
+    var allotTimes = [];
+
+    //calculate number of minutes to work on each project on day : FLOOR( remainting minutes / num days till due )
+    dayProjects.forEach(proj => {
+        var time_r = Math.floor(proj.timeRemaining);
+        var days_r = Math.round((new Date(proj.dueDate) - date) / (1000*60*60*24));
+        allotTimes.push((time_r / days_r) / 10);
+    });
+
+    //sort dayEvents so lower start times are first in array
+    dayEvents.sort(function(a, b) {
+        var sA = standard2Military(a.startTime);
+        var sB = standard2Military(b.startTime);
+
+        return (sA[0] * 60 + sA[1]) - (sB[0] * 60 + sB[1]);
+    });
+
+    var timeSlots = [];
+    //if no events on day then count whole day (8 am -> 11 pm) as a time slot
+    if(dayEvents.length == 0) {
+        timeSlots.push((standard2Military('11:00 PM')[0] * 60) - (standard2Military('8:00 AM')[0] * 60));
+    }
+
+    //calculate amount of time between each event
+    //if first event of day, calculate time between 8:00 AM and start time of first event
+    //for every other event, calculate time between last events end time, and next events start time
+    //for last event, calculate time between 11:00 PM and end time of last event
+    //all of the time slots are in the order they appear in the calendar
+    for(var i = 0; i < dayEvents.length; i++) {
+        if(i == 0) { //first event and 8:00 AM
+            var end = standard2Military(dayEvents[i].startTime);
+            var start = standard2Military("8:00 AM");
+
+            var diff = (end[0] * 60 + end[1]) - (start[0] * 60 + start[1]);
+            timeSlots.push(diff);
+        } else { //comparing events
+            var end = standard2Military(dayEvents[i].startTime);
+            var start = standard2Military(dayEvents[i-1].endTime);
+
+            var diff = (end[0] * 60 + end[1]) - (start[0] * 60 + start[1]);
+            timeSlots.push(diff);
+        }
+    }
+
+    //final edge case (last event end time to 11:00 PM)
+    if(dayEvents.length != 0) {
+        var lastEventEnd = standard2Military(dayEvents[dayEvents.length - 1].endTime);
+        var elevenPM = standard2Military("11:00 PM");
+
+        var lastDiff = (elevenPM[0] * 60 + elevenPM[1]) - (lastEventEnd[0] * 60 + lastEventEnd[1]);
+        timeSlots.push(lastDiff);
+    }
+
+    //combine projects with alloted times
+    var projectWithAllotedTimes = [];
+    for(var i = 0; i < allotTimes.length; i++) {
+        projectWithAllotedTimes.push({project: dayProjects[i], allotTime: allotTimes[i]});
+    }
+
+    //sort by alloted times (descending order so bigger projects come first)
+    projectWithAllotedTimes.sort(function(a, b) {
+        return b.allotTime - a.allotTime;
+    });
+
+    //place sorted values back into original arrays
+    for(var i = 0; i < projectWithAllotedTimes.length; i++) {
+        dayProjects[i] = projectWithAllotedTimes[i].project;
+        allotTimes[i] = projectWithAllotedTimes[i].allotTime;
+    }
+
+    //place projects into time slots
+    var projectsWithTimeSlotsAndTotalTime = []; //eww
+    //array for offsets to prevent projects overlapping
+    var offsets = new Array(timeSlots.length).fill(0);
+    for(var i = 0; i < dayProjects.length; i++) {
+        for(var j = 0; j < timeSlots.length; j++) {
+            //check if alloted time is greater than 30 min
+            //check if alloted time will fit into time slot (automatically handles negative timeslots)
+            if(allotTimes[i] >= 30 && allotTimes[i] <= timeSlots[j]) {
+                var et;
+                if(j == 0) {
+                    et = "8:00 AM";
+                } else {
+                    et = dayEvents[j].endTime;
+                }
+
+                projectsWithTimeSlotsAndTotalTime.push({project: dayProjects[i], 
+                                                        allotTime: allotTimes[i],
+                                                        startTime: et,
+                                                        offset: offsets[j]
+                                                    });
+                timeSlots[j] -= allotTimes[i];
+                offsets[j] += allotTimes[i];
+                break;
+            }
+        }
+    }
+    return projectsWithTimeSlotsAndTotalTime;   
+}
+
+function displayUserProjects(user) {
+    var startDate = getBeginningOfWeek(user.currentUserDate);
+    for(var i = 0; i < 7; i++) {
+        var ddd = new Date(startDate);
+        ddd.setDate(ddd.getDate() + i);
+        var dailyProjects = calcProjectTimeSlotsForDay(user, ddd);
+        dailyProjects.forEach(projectObj => {
+            var startTime = standard2Military(projectObj.startTime);
+            var startHour = startTime[0] + Math.floor(projectObj.offset / 60);
+            var startMin = startTime[1] + (projectObj.offset % 60);
+
+            var startTimeMin = startHour * 60 + startMin;
+
+            var endHour = Math.floor((startTimeMin + projectObj.allotTime) / 60);
+            var endMin =  Math.floor((startTimeMin + projectObj.allotTime) % 60);
+
+            var totalHours = endHour - startHour;
+
+            var totalSlots = totalHours + ((endMin > 0)?1:0);
+
+            for(j = 0; j < totalSlots; j++) {
+                var cell = document.getElementById("weekly-calendar").rows[startHour + j].cells[i+1];
+                cell.style.padding = "0px"; 
+                cell.style.margin = "0px";
+                var projDivContainer = document.createElement('div');
+                projDivContainer.className = 'project-container';
+            
+                var projDiv = document.createElement('div');
+                //eventDiv.style.height = "50%";
+                projDiv.id = projectObj.project.title + "-" + i;
+                projDiv.className = 'project ' + projDiv.id;
+
+                //rules of the road
+                if(j == 0) {
+                    projDivContainer.style.paddingTop = startMin / 3 + "%";
+                    if(startMin == 0) {
+                        projDivContainer.style.paddingTop = '1px';
+                    }
+                }
+
+                if(j == totalSlots - 1) {
+                    if(endMin == 0) {
+                        projDivContainer.style.paddingBottom = "1px";
+                    } else {
+                        projDivContainer.style.paddingBottom = totalHours * 10 + ((60 - endMin) / 3) + "%";
+                    }
+                }
+
+                if(j > 0) {
+                    projDiv.style.borderTopLeftRadius = '0px';
+                    projDiv.style.borderTopRightRadius = '0px';
+                    cell.style.borderTopWidth = '0px';
+                }
+                
+                if(totalSlots > 1 && j < totalSlots - 1) {
+                    projDiv.style.borderBottomLeftRadius = '0px';
+                    projDiv.style.borderBottomRightRadius = '0px';
+                    cell.style.borderBottomWidth = '0px';
+                }
+
+                /*
+                projDiv.onclick = function() { 
+                    //fill in edit modal with info
+                    $('#editProjectModal').modal('toggle');
+                    $('#e_title_edit').val(event.title);
+                    $('#est_edit').val(event.startTime);
+                    $('#eet_edit').val(event.endTime);
+                    $('#recurring_edit').prop('checked', event.recurring);
+                    $('#esd_edit').val(event.startDate);
+                    if(event.recurring) {
+                        $('#ed_edit').prop('disabled', false);
+                        $('#ed_edit').val(event.endDate);
+                        $('#rec_edit').prop('disabled', false);
+                        $('#rec_edit').val(event.recurrency);
+                        $('#timeframe_edit').prop('disabled', false);
+                        $('#timeframe_edit').val(event.timeframe);
+                    }
+                    $('#e_desc_edit').val(event.description);
+                    //store events id for backend usage
+                    $('#id-holder').text(event.id);
+                }
+                */
+
+                //in order to play the game, ya gotta know the rules
+                projDiv.onmouseover = function() {
+                    var e = document.getElementsByClassName(this.id);
+                    Array.prototype.forEach.call(e, function(el) {
+                        el.style.webkitFilter = "brightness(80%)";    
+                    });
+                }
+
+                projDiv.onmouseleave = function() {
+                    var e = document.getElementsByClassName(this.id);
+                    Array.prototype.forEach.call(e, function(el) {
+                        el.style.webkitFilter = "brightness(100%)";    
+                    });
+                }
+
+                //break it down, then build it back up
+                var title = document.createTextNode(projectObj.project.title);
+                projDiv.appendChild(title);
+                projDivContainer.appendChild(projDiv);
+                cell.childNodes[0].appendChild(projDivContainer);
+            }
+        });
+    }
+}
+
 //clears the calendar of all events and projects, resets the cells
 function clearAllEventsAndProjects() {
     $('.event').remove();
@@ -394,6 +611,16 @@ function updateUI() {
     displayDate(user); //display correct date in top left
     clearAllEventsAndProjects(); //reset calendar
     displayUsersEvents(user); //display all events in calendar
+    displayUserProjects(user);
     console.log(user); //log user status (for debugging)
     highlightDate(user); //highlight current date in top bar
+}
+
+/**
+ * Gets the date for the beginning of given dates week (sunday)
+ * @param {date to get week start of} date 
+ */
+function getBeginningOfWeek(date) {
+    var dateCopy = new Date(date);
+    return new Date(dateCopy.setDate(dateCopy.getDate() - dateCopy.getDay()));
 }
--- frontend/js/user/projects.js
+++ frontend/js/user/projects.js
@@ -1,9 +1,10 @@
-function Project(title, startDate, dueDate, expectedTimeHours, expectedTimeMinutes, description) {
+function Project(title, startDate, dueDate, expectedTimeHours, expectedTimeMinutes, description, timeRemaining) {
     this.title = title;
     this.startDate = startDate;
     this.dueDate = dueDate;
     this.expectedTimeHours = expectedTimeHours;
     this.expectedTimeMinutes = expectedTimeMinutes;
     this.description = description;
+    this.timeRemaining = timeRemaining;
     this.id = fastHash(this.title + this.startDate + this.description + this.dueDate);
 }
